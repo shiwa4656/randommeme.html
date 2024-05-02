d938cb5763a9cdfe5d7ba59719497a3378e2ed48
shirwac <shirwacstudy@gmail.com>
2024-05-03 00:55:16 +0200

    RandomMemeInitial Commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Function to fetch a random meme from the Imgflip API
+async function getMeme() {
+    try {
+        const response = await fetch('https://api.imgflip.com/get_memes');
+        const data = await response.json();
+        const memes = data.data.memes;
+        
+        // Get a random meme from the list
+        const randomIndex = Math.floor(Math.random() * memes.length);
+        const randomMeme = memes[randomIndex];
+
+        // Update the src attribute of the image with the meme URL
+        const memeImage = document.getElementById('meme');
+        memeImage.src = randomMeme.url;
+        memeImage.alt = randomMeme.name;
+    } catch (error) {
+        console.error('Error fetching meme:', error);
+    }
+}
+
+
+setInterval(getMeme, 5000);
+function white(){
+    document.body.style.backgroundColor = "white";
+}
+function black(){
+    document.body.style.backgroundColor = "black";
+}
+function blue(){
+    document.body.style.backgroundColor = "blue";
+}
